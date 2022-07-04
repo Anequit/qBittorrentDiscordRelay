700bf4c950e82c24ff86c69526ff58ac9ed49fcd
Bailey <Anequit@gmail.com>
2022-07-04 01:14:54 -0400

    Add MessageWebhookAsync

---
+++ src/qBittorrentDiscordRelayCLI/HttpClientExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace qBittorrentDiscordRelayCLI;
+
+public static class HttpClientExtensions
+{
+	public static async Task MessageWebhookAsync(this HttpClient client, string webhook, string message)
+	{
+        await client.PostAsJsonAsync(webhook, new Dictionary<string, string>()
+        {
+            { 
+                "content", message 
+            }
+        });
+    }
+}
